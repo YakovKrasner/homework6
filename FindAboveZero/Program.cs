bff37ed3aec7bfcf392063d14e7e5d21468989d3
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-30 22:16:31 +0700

    Initial commit_Count positive numbers

---
+++ FindAboveZero/Program.cs
@@ -0,0 +1,14 @@
+﻿int NumbersAmount = 7;
+int [] Array = new int [NumbersAmount];
+int PositiveNumberAmount = 0;
+for (int i = 0; i < Array.Length; i++)
+    {
+Console.WriteLine ($"Введите элемент массива под номером {i}");
+Array [i] = Convert.ToInt32(Console.ReadLine ());
+    }
+for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array [i] > 0) PositiveNumberAmount ++;
+    }
+Console.WriteLine ($"Количество чисел больше ноля равно {PositiveNumberAmount}");
+
